3ec380a0e8034ce205d16ae9013d61bbe8bfdb99
dflowers7 <dflowers@mit.edu>
2016-11-11 15:14:27 -0500

    Fixed problems when bounds are set to 0 or Inf
    Samples between 1e-6 and 1e6 when 0 or Inf is detected.

--- chooseInitialParametersByCurvature.m
+++ chooseInitialParametersByCurvature.m
@@ -14,6 +14,16 @@ nParams = m.nk;
 pLo = fitopts.LowerBound;
 pHi = fitopts.UpperBound;
 
+% Fix 0 or Inf bounds
+if any(pLo == 0)
+    warning('Zero-valued lower bounds detected. Assuming a 1e-6 lower bound for log-space sampling.')
+    pLo(pLo == 0) = 1e-6;
+end
+if any(isinf(pHi))
+    warning('Inf-valued upper bounds detected. Assuming a 1e6 upper bound for log-space sampling.')
+    pHi(isinf(pHi)) = 1e6;
+end
+
 nParamTrys = 100*nParamSets;
 p = randomlySampleParameters(nParams,nParamTrys,pLo,pHi);
 
